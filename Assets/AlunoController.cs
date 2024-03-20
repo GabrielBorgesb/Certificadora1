076c5f98c6c36af206f0152c7a85cba06f628bbb
GabrielBorgesb <98597634+GabrielBorgesb@users.noreply.github.com>
2024-03-20 00:17:38 -0300

    Att

--- Assets/AlunoController.cs
+++ Assets/AlunoController.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class AlunoController : MonoBehaviour
 {
+    //Stats
     public int pontuacao;
 
+    //Inputs
+    public string respostaInput;
+
+    //Controladores
+    public int questaoAtual;
 
     void Start()
     {
@@ -15,8 +21,31 @@ public class AlunoController : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.F)){
+            
+            
+        }
+    }
+
+    public void UpdateInput(string input)
+    {
+        respostaInput = input;
+    }
+
+    
+    //Ações
+    public void EnviarResposta()
+    {
+        //Acertou
+        if(respostaInput == UIController.instance.bdQuestoes.questoes[questaoAtual].resposta)
+        {
             pontuacao += 100;
             UIController.instance.UpdatePontuacao(pontuacao);
         }
+        else
+        {
+
+        }
+
     }
+
 }
--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -8,18 +8,20 @@ public class UIController : MonoBehaviour
 {
     public static UIController instance;
 
-    [SerializeField] private BDQuestoes bdQuestoes;
+    public BDQuestoes bdQuestoes;
+    [SerializeField] private AlunoController aluno;
 
 
     //UI Geral
     [SerializeField] private TMP_Text pontuacaoUI;
 
-    //UI Seleção de Questão
+    //UI Questão
     [SerializeField] private TMP_Text questaoText;
     [SerializeField] private Image questaoImagem;
+    
 
     private void Awake() {
-        if(instance == null){
+        if(instance == null && instance != this){
             instance = this;
         }else
         {
@@ -38,5 +40,8 @@ public class UIController : MonoBehaviour
     public void UpdateQuestao(int questaoIndex){
         questaoText.SetText(bdQuestoes.questoes[questaoIndex].enunciado);
         questaoImagem.sprite = bdQuestoes.questoes[questaoIndex].imagem;
+        aluno.questaoAtual = questaoIndex;
     }
+
+    
 }
